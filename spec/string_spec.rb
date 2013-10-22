57a0c51de4d2365b9d10a4cb4c06f64aa1154718
Vincent <vincent.pretre@smartesting.com>
2013-10-22 10:49:30 +0200

    Extended string class with some utilities

---
+++ spec/string_spec.rb
@@ -0,0 +1,64 @@
+# encoding: UTF-8
+
+require_relative '../string'
+
+describe String do
+  context 'literate' do
+    it 'returns the same string with only ascii characters' do
+      'Skøl ofenstrü'.literate.should eq('Skol ofenstru')
+    end
+  end
+
+  context 'normalize' do
+    it 'trims the string' do
+      '     lala     '.normalize.should eq('lala')
+    end
+
+    it 'replaces multiple white characters by a single underscore' do
+      "flip  flap\tdie\n\nGiraffe".normalize.should eq('flip_flap_die_Giraffe')
+    end
+
+    it 'removes quotes' do
+      %|it's a "string"|.normalize.should eq('its_a_string')
+    end
+
+    it 'literates the string' do
+      'Skøl ofenstrü'.normalize.should eq('Skol_ofenstru')
+    end
+
+    it 'does all at once, hurray <o/' do
+      %|  it's  the Støry of\n\n"Pouin Pouin le Marsouin"\n  |.normalize.should eq(
+        'its_the_Story_of_Pouin_Pouin_le_Marsouin')
+    end
+  end
+
+  context 'underscore' do
+    it 'returns the string in snake_case' do
+      'SnakeCase'.underscore.should eq('snake_case')
+      'almostSnake_case_string'.underscore.should eq('almost_snake_case_string')
+    end
+
+    it 'leaves intact a string already in snake case' do
+      'my_snake_case_string'.underscore.should eq('my_snake_case_string')
+    end
+
+    it 'normalize the string before' do
+      'Skøl ofenstrü'.underscore.should eq('skol_ofenstru')
+    end
+  end
+
+  context 'camelize' do
+    it 'returns the string in CamelCase' do
+      'camel_case'.camelize.should eq('CamelCase')
+      'almostCamelCaseString'.camelize.should eq('AlmostCamelCaseString')
+    end
+
+    it 'leaves intact a string already camelized' do
+      'ACamelCaseString'.camelize.should eq('ACamelCaseString')
+    end
+
+    it 'normalize the string before' do
+      'Skøl ofenstrü'.camelize.should eq('SkolOfenstru')
+    end
+  end
+end
---
+++ string.rb
@@ -0,0 +1,29 @@
+require 'i18n'
+
+require_relative 'utils'
+
+class String
+  def literate
+    I18n.transliterate(self)
+  end
+
+  def normalize
+    literated = self.literate
+    literated.strip.gsub(/\s+/, '_').gsub(/\W/, '')
+  end
+
+  def underscore
+    # based on:
+    # http://stackoverflow.com/questions/1509915/converting-camel-case-to-underscore-case-in-ruby
+    normalized = self.normalize
+    normalized.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+  end
+
+  def camelize
+    normalized = self.normalize
+    normalized.split('_').map {|w| "#{w[0].upcase}#{w[1..-1]}"}.join
+  end
+end
