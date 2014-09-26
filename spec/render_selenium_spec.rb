8953c5379171b3a57d9c8b9c8821946a39a02a0a
Vincent Pretre <v.pretre@smartesting.com>
2014-09-26 16:46:12 +0200

    Added tests for Selenium export

---
+++ spec/render_selenium_spec.rb
@@ -0,0 +1,111 @@
+require_relative 'spec_helper'
+require_relative '../lib/zest-publisher/nodes'
+
+describe 'Selenium IDE rendering' do
+  # Note: we do not want to test everything as we'll only render
+  # tests and calls.
+
+  before(:each) do
+    @first_test = Zest::Nodes::Test.new(
+      'Login',
+      '',
+      [],
+      [
+        Zest::Nodes::Call.new('open', [
+          Zest::Nodes::Argument.new('target', Zest::Nodes::StringLiteral.new('/login'))
+        ]),
+        Zest::Nodes::Call.new('type', [
+          Zest::Nodes::Argument.new('target', Zest::Nodes::StringLiteral.new('id=login')),
+          Zest::Nodes::Argument.new('value', Zest::Nodes::StringLiteral.new('user@example.com'))
+        ]),
+        Zest::Nodes::Call.new('type', [
+          Zest::Nodes::Argument.new('target', Zest::Nodes::StringLiteral.new('id=password')),
+          Zest::Nodes::Argument.new('value', Zest::Nodes::StringLiteral.new('s3cret'))
+        ]),
+        Zest::Nodes::Call.new('click', [
+          Zest::Nodes::Argument.new('target', Zest::Nodes::StringLiteral.new('css=.login-form input[type=submit]'))
+        ]),
+        Zest::Nodes::Call.new('verifyTextPresent', [
+          Zest::Nodes::Argument.new('value', Zest::Nodes::StringLiteral.new('Welcome user !'))
+        ])
+      ])
+
+
+    @tests = Zest::Nodes::Tests.new([@first_test])
+    @first_test.parent = @tests
+    @tests.parent = Zest::Nodes::Project.new('My test project')
+
+    @context = {framework: '', forced_templates: {}}
+  end
+
+  context 'Test' do
+    it 'generates an html file' do
+      @context[:forced_templates] = {'test' => 'single_test'}
+
+      expect(@first_test.render('selenium', @context)).to eq([
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+        '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">',
+        ' <head profile="http://selenium-ide.openqa.org/profiles/test-case">',
+        ' <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />',
+        ' <link rel="selenium.base" href="http://selenium.googlecode.com/" />',
+        ' <title>New Test</title>',
+        '</head>',
+        '<body>',
+        '  <table cellpadding="1" cellspacing="1" border="1">',
+        '    <thead>',
+        '      <tr>',
+        '        <td rowspan="1" colspan="3">Login</td>',
+        '      </tr>',
+        '    </thead>',
+        '    <tbody>',
+        '      <tr>',
+        '        <td>open</td>',
+        '        <td>/login</td>',
+        '        <td></td>',
+        '      </tr>',
+        '      <tr>',
+        '        <td>type</td>',
+        '        <td>id=login</td>',
+        '        <td>user@example.com</td>',
+        '      </tr>',
+        '      <tr>',
+        '        <td>type</td>',
+        '        <td>id=password</td>',
+        '        <td>s3cret</td>',
+        '      </tr>',
+        '      <tr>',
+        '        <td>click</td>',
+        '        <td>css=.login-form input[type=submit]</td>',
+        '        <td></td>',
+        '      </tr>',
+        '      <tr>',
+        '        <td>verifyTextPresent</td>',
+        '        <td>Welcome user !</td>',
+        '        <td></td>',
+        '      </tr>',
+        '    </tbody>',
+        '  </table>',
+        '</html>'
+      ].join("\n"))
+    end
+  end
+
+  context 'Tests' do
+    it 'generates a summary' do
+      expect(@tests.render('selenium', @context)).to eq([
+        '<html>',
+        '  <head>',
+        '    <title>My test project</title>',
+        '  </head>',
+        '  <body>',
+        '    <table>',
+        '      <tr><td><b>Suite Of Tests</b></td></tr>',
+        '      <tr><td><a href="./Login.html">Login</a></td></tr>',
+        '    </table>',
+        '  </body>',
+        '</html>'
+      ].join("\n"))
+    end
+  end
+end
