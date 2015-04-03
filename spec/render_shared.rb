a833a80b23b25c9d5f972d2e78d5d38199e82768
Vincent Pretre <v.pretre@smartesting.com>
2015-04-03 11:19:40 +0200

    Ruby export: all arguments must be set Crappy monkey patching in render shared so all_arguments is also set

--- spec/render_shared.rb
+++ spec/render_shared.rb
@@ -3,6 +3,25 @@ require_relative '../lib/hiptest-publisher/parameter_type_adder'
 require_relative '../lib/hiptest-publisher/nodes'
 
 shared_context "shared render" do
+
+  before(:all) do
+    class Hiptest::Nodes::Call < Hiptest::Nodes::Node
+      def initialize(actionword, arguments = [])
+        super()
+        @children = {:actionword => actionword, :arguments => arguments, :all_arguments => arguments}
+      end
+    end
+  end
+
+  after(:all) do
+    class Hiptest::Nodes::Call < Hiptest::Nodes::Node
+      def initialize(actionword, arguments = [])
+        super()
+        @children = {:actionword => actionword, :arguments => arguments}
+      end
+    end
+  end
+
   before(:each) do
     @null = Hiptest::Nodes::NullLiteral.new
     @what_is_your_quest = Hiptest::Nodes::StringLiteral.new("What is your quest ?")
@@ -255,6 +274,7 @@ shared_context "shared render" do
           Hiptest::Nodes::Argument.new('path', Hiptest::Nodes::StringLiteral.new('/login')
         )])
       ])
+    @second_test
 
     @tests = Hiptest::Nodes::Tests.new([@first_test, @second_test])
     @first_test.parent = @tests
