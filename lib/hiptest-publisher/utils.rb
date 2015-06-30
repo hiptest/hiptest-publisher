b1c1ea9ff462ec1745e49a5a249dd162fe37323e
Christophe Bliard <christophe.bliard@smartesting.com>
2015-06-30 15:07:08 +0200

    strip version to remove linefeed character

--- lib/hiptest-publisher/utils.rb
+++ lib/hiptest-publisher/utils.rb
@@ -12,7 +12,7 @@ end
 def hiptest_publisher_version
   Gem.loaded_specs['hiptest-publisher'].version.to_s
 rescue
-  File.read("#{hiptest_publisher_path}/VERSION") if File.exists?("#{hiptest_publisher_path}/VERSION")
+  File.read("#{hiptest_publisher_path}/VERSION").strip if File.exists?("#{hiptest_publisher_path}/VERSION")
 end
 
 def pluralize(count, singular)
