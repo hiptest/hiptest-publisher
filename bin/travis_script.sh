2b10907d826d4bda300429a2d4257b98ed6e25fa
Christophe Bliard <christophe.bliard@trux.info>
2018-04-25 09:20:49 +0200

    More verbose test output
    So we know why we have this hiptest-publisher output in the ci logs.

--- bin/travis_script.sh
+++ bin/travis_script.sh
@@ -1,4 +1,6 @@
 gem install hiptest-publisher
+echo "== Running hiptest-publisher --help"
 hiptest-publisher --help
 
+echo "== Running rspec"
 CODECLIMATE_REPO_TOKEN=4c579687b0754f976610c89a4ef11c1c95c10b0ee378597d6ddd646fab8af06c bundle exec rspec spec
