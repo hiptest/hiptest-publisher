537f2e3d9d32c1946d8b838e7fc3c5eb84f4115d
Davert <davert.ua@gmail.com>
2019-08-06 00:37:30 +0300

    fixing tests

--- spec/render/codeceptjs_spec.rb
+++ spec/render/codeceptjs_spec.rb
@@ -19,7 +19,11 @@ describe 'Cucumber/Javascript rendering' do
     let(:rendered_actionwords) {
       [
         'const { I } = inject();',
-        'const actionwords = Object.create(require(\'./actionwords.js\')).Actionwords;',
+        'let actionwords;'
+        ''
+        'Before(() => {'
+        '    actionwords = Object.create(require(\'./actionwords.js\')).Actionwords;'
+        '});'
         '',
         'Given(/^the color "(.*)"$/, function (color) {',
         '    return actionwords.theColorColor(color);',
